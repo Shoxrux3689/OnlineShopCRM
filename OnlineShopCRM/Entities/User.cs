c6aa3cd73209cf1fe34aa2ca71c1889f974a37ae
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-08-21 22:34:05 +0500

    added user properties

--- OnlineShopCRM/Entities/User.cs
+++ OnlineShopCRM/Entities/User.cs
@@ -1,6 +1,9 @@
-﻿namespace OnlineShopCRM.Entities
+﻿namespace OnlineShopCRM.Entities;
+
+public class User
 {
-    public class User
-    {
-    }
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    public string? PasswordHash { get; set; }
+    public List<Customer>? Customers { get; set; }
 }
