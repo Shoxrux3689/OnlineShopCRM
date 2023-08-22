7e6ba6ac2b40ffa65328e7869feb57e87720f389
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-08-22 22:53:26 +0500

    added customer controller

---
+++ OnlineShopCRM/Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopCRM.Repositories.Interfaces;
+
+namespace OnlineShopCRM.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomersController : ControllerBase
+{
+    private readonly ICustomerManager customerManager;
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetCustomers(CustomerFilter customerFilter)
+    {
+        return Ok();
+    }
+}
