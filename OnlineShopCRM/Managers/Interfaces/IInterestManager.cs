3990b08718ed313190c59ed4f750c24c4a86ace9
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-08-25 02:30:22 +0500

    added interest interface

--- OnlineShopCRM/Managers/Interfaces/IInterestManager.cs
+++ OnlineShopCRM/Managers/Interfaces/IInterestManager.cs
@@ -1,5 +1,13 @@
-﻿namespace OnlineShopCRM.Managers.Interfaces;
+﻿using OnlineShopCRM.Entities;
+using OnlineShopCRM.Models.InterestModels;
+
+namespace OnlineShopCRM.Managers.Interfaces;
 
 public interface IInterestManager
 {
+    Task CreateInterest(CreateInterestModel createInterestModel);
+    Task<Interest?> GetInterestByCustomerId(int customerId);
+    Task<Interest?> GetInterestById(int interestId);
+    Task<List<Interest>?> GetInterestsByCustomerId(int customerId);
+    Task UpdateInterest(int customerId, UpdateInterestModel updateInterestModel);
 }
---
+++ OnlineShopCRM/Models/InterestModels/CreateInterestModel.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShopCRM.Entities;
+
+namespace OnlineShopCRM.Models.InterestModels;
+
+public class CreateInterestModel
+{
+    public required string InterestDescription { get; set; }
+    public int CustomerId { get; set; }
+    public DateOnly? Date { get; set; }
+    public required string HashTags { get; set; }
+    public bool IsActual { get; set; }
+}
