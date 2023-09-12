9a826f926c627e1dd0319c8054032726b2b1e16e
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-09-12 18:56:09 +0500

    added actions to sales controller

--- OnlineShopCRM/Controllers/SalesController.cs
+++ OnlineShopCRM/Controllers/SalesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShopCRM.Filters;
 using OnlineShopCRM.Managers.Interfaces;
 using OnlineShopCRM.Models.SaleModels;
 
@@ -30,6 +31,7 @@ public class SalesController : ControllerBase
         }
     }
 
+    [HttpGet("{saleId}")]
     public async Task<IActionResult> GetSaleById(int customerId, int saleId)
     {
         var sale = await _saleManager.GetSaleById(saleId);
@@ -39,5 +41,19 @@ public class SalesController : ControllerBase
         }
 
         return Ok(sale);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetSalesByCustomerId(int customerId)
+    {
+        var sales = await _saleManager.GetSalesByCustomerId(customerId);
+
+        return Ok(sales);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetSalesByFilter(SaleFilter saleFilter)
+    {
+
     }
 }
