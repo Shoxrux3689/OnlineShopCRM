2ee1261db030af99abf33c055034293ae588822d
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-09-13 14:20:47 +0500

    added account controller

--- OnlineShopCRM/Controllers/AccountController.cs
+++ OnlineShopCRM/Controllers/AccountController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShopCRM.Managers;
+using OnlineShopCRM.Managers.Interfaces;
+using OnlineShopCRM.Models.UserModels;
 
 namespace OnlineShopCRM.Controllers;
 
@@ -7,5 +11,45 @@ namespace OnlineShopCRM.Controllers;
 [ApiController]
 public class AccountController : ControllerBase
 {
-    private readonly 
+    private readonly IUserManager _userManager;
+
+    public AccountController(IUserManager userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpGet("register")]
+    public async Task<IActionResult> Registration(CreateUserModel createUserModel)
+    {
+        try
+        {
+            var id = await _userManager.Registration(createUserModel);
+            return Created("", new { Id = id });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet("login")]
+    public async Task<IActionResult> Login(LoginUserModel loginUserModel)
+    {
+        try
+        {
+            var token = await _userManager.Login(loginUserModel);
+            return Ok(new { Token = token });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [Authorize]
+    [HttpGet("profile")]
+    public async Task<IActionResult> Profile()
+    {
+        //httpcontext dan id sini olish kere
+    }
 }
