bd3b588749c791a092be7954d643e82df3827615
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-09-18 16:06:48 +0500

    added order entity

---
+++ OnlineShopCRM/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShopCRM.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsPay { get; set; }
+    public int Price { get; set; }
+    public DateOnly OrderDate { get; set; }
+    public 
+}
