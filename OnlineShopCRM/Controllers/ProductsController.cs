5674b206d2f70464696849937302a77bb4c2990c
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-09-21 14:56:10 +0500

    added product controller

---
+++ OnlineShopCRM/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopCRM.Managers.Interfaces;
+
+namespace OnlineShopCRM.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductManager _productManager;
+
+    public ProductsController(IProductManager productManager)
+    {
+        _productManager = productManager;
+    }
+
+    [HttpGet("{productId}")]
+    public async Task<IActionResult> GetProductById(int productId)
+    {
+        try
+        {
+            var product = await _productManager.GetProductById(productId);
+            if (product == null)
+                return NotFound("Product is not found!");
+
+            return Ok(product);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
