d10d76208195b7169775b88174332b0c4094b4c6
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-09-22 16:44:55 +0500

    added cutomer product repository

---
+++ OnlineShopCRM/Repositories/CustomerProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineShopCRM.Context;
+
+namespace OnlineShopCRM.Repositories;
+
+public class CustomerProductRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public CustomerProductRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+}
---
+++ OnlineShopCRM/Repositories/Interfaces/ICustomerProductRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShopCRM.Repositories.Interfaces;
+
+public interface ICustomerProductRepository
+{
+    Task AddCustomerProduct(int customerId, int productId);
+    Task UpdateCustomerProduct(int customerId, int productId);
+
+}
